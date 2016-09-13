77725740601fda85e87e09488f075e884e6effed
yasaichi <yasaichi@users.noreply.github.com>
2016-09-14 00:24:00 +0900

    Setup SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,16 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require "simplecov"
+require "codeclimate-test-reporter"
+
+SimpleCov.start do
+  add_filter "/spec/"
+
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ])
+end
 
 require "grease"
 require "sprockets"
