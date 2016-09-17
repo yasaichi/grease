f348e9751ebee483ce76f5ec8cbd19e4eff78671
yasaichi <yasaichi@users.noreply.github.com>
2016-09-17 12:34:12 +0900

    Rename "engine" with "template_class"

--- lib/grease/adapter.rb
+++ lib/grease/adapter.rb
@@ -1,12 +1,12 @@
 module Grease
   class Adapter
-    def initialize(engine)
-      @engine = engine
+    def initialize(template_class)
+      @template_class = template_class
     end
 
     def call(input)
       context = input[:environment].context_class.new(input)
-      template = @engine.new { input[:data] }
+      template = @template_class.new { input[:data] }
 
       # TODO: Hack for converting ActiveSupport::SafeBuffer into String
       output = "#{template.render(context, {})}"
