301aab21a85daac793334907ff891cb31e3453b1
yasaichi <yasaichi@users.noreply.github.com>
2016-09-17 13:22:57 +0900

    Fix TypeError raised by ActiveSupport::SafeBuffer

--- lib/grease/adapter.rb
+++ lib/grease/adapter.rb
@@ -7,8 +7,9 @@ module Grease
     def call(input)
       context = input[:environment].context_class.new(input)
       template = @engine.new { input[:data] }
-      output = template.render(context, {})
 
+      # TODO: Hack for converting ActiveSupport::SafeBuffer into String
+      output = "#{template.render(context, {})}"
       context.metadata.merge(data: output)
     end
   end
