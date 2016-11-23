cfa6e1c621750ce08a5911dbe9b0f7f2d15bb115
yasaichi <yasaichi@users.noreply.github.com>
2016-11-23 21:02:59 +0900

    Fix an error caused by codeclimate-test-reporter v1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,15 +17,9 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require "simplecov"
-require "codeclimate-test-reporter"
 
 SimpleCov.start do
   add_filter "/spec/"
-
-  formatter SimpleCov::Formatter::MultiFormatter.new([
-    SimpleCov::Formatter::HTMLFormatter,
-    CodeClimate::TestReporter::Formatter
-  ])
 end
 
 require "grease"
